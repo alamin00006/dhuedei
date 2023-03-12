05a3f6c736719fb58439d17258c1dd6916e7c8ae
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-12 17:50:51 +0600

    table add

--- src/views/Services/Services.js
+++ src/views/Services/Services.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./Services.css";
-import pricListImg from "../../images/price-list1.png";
+// import pricListImg from "../../images/price-list1.png";
+import PriceTable from "../PriceTable/PriceTable";
 const Services = () => {
   return (
-    <div className="mx-44">
+    <div className="w-4/5 m-0 mx-auto">
       <h1 className="text-3xl text-cyan-500 font-bold">Services & Pricing</h1>
       <div className="divider before:bg-cyan-500 after:bg-cyan-500 mt-8 font-bold text-xl text-cyan-600">
         Price Table
       </div>
       <div className="flex justify-center">
-        <div>
+        {/* <div>
           <img className="w-full" src={pricListImg} alt="" />
-        </div>
+        </div> */}
       </div>
+      <PriceTable></PriceTable>
     </div>
   );
 };
